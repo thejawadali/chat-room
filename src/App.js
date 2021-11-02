a021c8c78c2bd12f28eefc655fc782e6be18a321
Jawad Ali <alijamil734@gmail.com>
2021-11-02 22:09:11 +0500

    made input field for message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function App () {
+ const [msgText, setMsgText]  = useState("")
+  function sendMessage(event) {
+    event.preventDefault()
+    console.log(msgText);
+    setMsgText("")
+  }
+
   return (
-    <div className="App">
+    <div className="w-full h-screen bg-yellow-50 flex justify-center">
+      {/* Container starts here */}
+      <div className="border w-full text-center">
+        {/* Input field starts here */}
+        <form onSubmit={sendMessage} className="fixed bottom-3 right-10 left-10 flex bg-gray-200 mx-auto rounded-xl overflow-hidden">
+          <input value={msgText} onChange={(e)=> {setMsgText(e.target.value)}} placeholder="Type a message and press enter" className="bg-transparent flex-1 p-3 text-lg outline-none" type="text" />
+          <button className="bg-blue-600 text-white px-5">Send</button>
+        </form>
+        {/* Input field starts here */}
+      </div>
+      {/* Container Ends here */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

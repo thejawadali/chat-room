3ba6c9bdb594824f0852b27832492b78d99c4671
Jawad Ali <alijamil734@gmail.com>
2021-11-07 14:56:20 +0500

    TODO: fix fixed positioned objects

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ function App () {
   return (
     <div className="max-w-3xl bg-gray-800 mx-auto min-h-screen">
       {/* Header Start Here */}
-      <header className="w-full text-center bg-purple-500 py-6  box-border">
+      <header className="w-full text-center bg-purple-500 py-6  box-border fixed top-0 left-0 right-0 z-10">
         {/* <header className="w-full flex justify-center items-center h-24 bg-purple-700"> */}
         <h1 className="text-white text-2xl cursor-pointer">Chat Room</h1>
       </header>
       {/* Header Ends here */}
 
       {/* Main Starts here */}
-      <main className="px-5 pb-16">
+      <main className="px-5 pb-16 relative top-12">
         {localStorage.getItem( "name" ) ? <ChatRoom /> :
           <div className="w-full bg-yellow-500 flex justify-center items-center">
             <form onSubmit={saveName} className="w-full bg-green-500 flex flex-col">
